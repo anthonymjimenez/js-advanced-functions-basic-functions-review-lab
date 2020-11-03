0ac9f02ae1554476318e4662c2717632911ef2ee
anthonymjimenez <anthonyjim96@gmail.com>
2020-11-02 22:41:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const saturdayFun = (activity='roller-skate') => 
+    `This Saturday, I want to ${activity}!`
+    
+const mondayWork =  (activity='go to the office') => 
+    `This Monday, I will ${activity}.`
+
+const wrapAdjective = (opr="*") =>
+        (str = 'special') => `You are ${opr}${str}${opr}!` 
+
+const Calculator = {
+    add: (x,y) => x+y,
+    subtract: (x,y) => x-y,
+    multiply: (x,y) => x*y,
+    divide: (x,y) => x/y
+ }
+
+const actionApplyer = (startInt, arrayOfCallbacks) => {
+     if(arrayOfCallbacks.length !== 0) arrayOfCallbacks.forEach(callback => startInt = callback(startInt)) 
+     return startInt
+}
